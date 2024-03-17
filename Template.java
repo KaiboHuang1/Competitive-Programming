67ababfe0f711f5173f41e03624973dcf3f2b6fc
KaiboHuang1 <99372604+KaiboHuang1@users.noreply.github.com>
2024-03-16 20:36:16 -0400

    Update Template.java
    This is my updated template for competitive programming

--- Template.java
+++ Template.java
@@ -8,6 +8,7 @@ public class Template {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static PrintWriter out = new PrintWriter(System.out);
+	static int MAX = Integer.MAX_VALUE;
 
 	public static void main(String[] args) throws IOException {
 
@@ -135,11 +136,29 @@ public class Template {
 	public static void sort(int[] arr) {
 
 		ArrayList<Integer> ls = new ArrayList<Integer>();
-		for (int x : arr)
+		for (int x : arr) {
 			ls.add(x);
+		}
+		
 		Collections.sort(ls);
-		for (int i = 0; i < arr.length; i++)
+		for (int i = 0; i < arr.length; i++) {
 			arr[i] = ls.get(i);
+		}
+	}
+	public static boolean isPrime(long n) {
+		if (n < 2)
+			return false;
+		if (n == 2 || n == 3)
+			return true;
+		if (n % 2 == 0 || n % 3 == 0)
+			return false;
+		long sqrtN = (long) Math.sqrt(n) + 1;
+		for (long i = 6L; i <= sqrtN; i += 6) {
+			if (n % (i - 1) == 0 || n % (i + 1) == 0)
+				return false;
+		}
+		return true;
 	}
 }
+
 
